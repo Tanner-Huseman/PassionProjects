f3e0e82373b0694fc1f71bd418bcf1010d4df7a6
Tanner-Huseman <100720193+Tanner-Huseman@users.noreply.github.com>
2022-03-01 14:09:07 -0500

    QWIXX scorecard app

---
+++ QWIXX/QWIXX/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QWIXX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LETS START RECORDING A GAME OF QWIXX");
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+
+            ScoreCard scoreCard = new ScoreCard(name);
+            scoreCard.Run();
+        }
+    }
+}
